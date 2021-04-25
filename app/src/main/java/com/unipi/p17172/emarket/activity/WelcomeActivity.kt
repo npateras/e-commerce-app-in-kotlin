5789689e6f7a2b0a7348fc6ac4bd6ea065982c47
Nikolas P <nikolas.98@live.com>
2021-04-25 15:22:56 +0300

    Initial commit

---
+++ app/src/main/java/com/unipi/p17172/emarket/activity/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.unipi.p17172.emarket.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.unipi.p17172.emarket.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
